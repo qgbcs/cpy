bfe85e908938911dc8b5cfd02f7ecc10f43fdf47
qgb <qgbcs1@gmail.com>
2022-07-24 01:11:23 +0800

    msvc_link_pyd.bat

---
+++ msvc_link_pyd.bat
@@ -0,0 +1,7 @@
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29910\bin\HostX86\x64\link.exe" /nologo /INCREMENTAL:NO /LTCG /nodefaultlib:libucrt.lib ucrt.lib /DLL /MANIFEST:EMBED,ID=2 /MANIFESTUAC:NO /LIBPATH:C:\QGB\Anaconda3\libs /LIBPATH:C:\QGB\Anaconda3\PCbuild\amd64 "/LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29910\ATLMFC\lib\x64" "/LIBPATH:C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.28.29910\lib\x64" "/LIBPATH:C:\Program Files (x86)\Windows Kits\NETFXSDK\4.8\lib\um\x64" "/LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\ucrt\x64" "/LIBPATH:C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\um\x64" /EXPORT:PyInit_fputs build\temp.win-amd64-3.7\Release\fputsmodule.obj /OUT:build\lib.win-amd64-3.7\fputs.cp37-win_amd64.pyd /IMPLIB:build\temp.win-amd64-3.7\Release\fputs.cp37-win_amd64.lib
+
+@REM create:
+REM C:\test\github\cpy_fputs\build\temp.win-amd64-3.7\Release\fputs.cp37-win_amd64.exp
+REM C:\test\github\cpy_fputs\build\lib.win-amd64-3.7\fputs.cp37-win_amd64.pyd
+
+@REM
